205653ff98f4cb0e281622846f87a92bc51c381b
MathisBls <mathis.bls@pm.me>
2026-06-14 19:02:21 +0200

    add electric blue framework accent overlay

--- products/quorin_sdk_phone_x86_64.mk
+++ products/quorin_sdk_phone_x86_64.mk
@@ -4,3 +4,7 @@ $(call inherit-product, vendor/lineage/build/target/product/lineage_sdk_phone_x8
 # Quorin product identity
 PRODUCT_NAME := quorin_sdk_phone_x86_64
 PRODUCT_MODEL := QuorinOS Android SDK built for x86_64
+
+# Quorin theme overlays
+PRODUCT_PACKAGES += \
+    QuorinFrameworkOverlay
